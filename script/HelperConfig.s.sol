cea1fd3b84ab477e48c825ba61fb52d218218b51
diasfusuy <diasfusuy4@gmail.com>
2025-12-10 15:32:01 -0600

    Added anvil config into the helperConfig file

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -8,6 +8,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 contract HelperConfig is Script {
     // If we are on local anvil, we deploy mocks
     // Otherwise, grab the existing address from the live network
@@ -44,8 +45,18 @@ contract HelperConfig is Script {
         return ethConfig;      
     }
 
-    function getAnvilEthConfig() public pure returns(NetworkConfig memory) {
+    function getAnvilEthConfig() returns(NetworkConfig memory) {
         // price feed address
+        // 1. Deploy the mocks
+        // 2. Return the mock address
 
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfg memory anvilConfig = NetworkConfg({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
     }
 }

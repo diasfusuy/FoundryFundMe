0331cc5482bcb0293c37ea5d5b43ef8a7239b165
diasfusuy <diasfusuy4@gmail.com>
2025-12-12 17:25:58 -0600

    Addded constants

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,6 +15,9 @@ contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
     struct NetworkConfig {
         address priceFeed; // ETH/USD price feed address
     }
@@ -25,7 +28,7 @@ contract HelperConfig is Script {
         } else if (block.chainid == 1) {
             activeNetworkConfig = getMainnetEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -45,16 +48,23 @@ contract HelperConfig is Script {
         return ethConfig;      
     }
 
-    function getAnvilEthConfig() returns(NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+        if(activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
         // price feed address
         // 1. Deploy the mocks
         // 2. Return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS, 
+            INITIAL_PRICE
+            );
         vm.stopBroadcast();
 
-        NetworkConfg memory anvilConfig = NetworkConfg({
+        NetworkConfig memory anvilConfig = NetworkConfig({
             priceFeed: address(mockPriceFeed)
         });
         return anvilConfig;

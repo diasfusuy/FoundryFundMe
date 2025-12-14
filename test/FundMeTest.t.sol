f306fb5de34a77f754a7eba54c42cf7af084adb4
diasfusuy <diasfusuy4@gmail.com>
2025-12-14 15:33:19 -0600

    Added more test coverage. Also, used some foundry cheatcodes such as; vm.expectrevert, vm.prank, vm.deal, makeAddr()

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -6,19 +6,24 @@ import {FundMe} from "../src/FundMe.sol";
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 contract FundMeTest is Test {
   FundMe fundMe;
+
+  address USER = makeAddr("user");
+  uint256 constant SEND_VALUE = 0.1 ether;
+  uint256 constant STARTING_BALANCE = 10 ether;
   function setUp() external{
     // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     DeployFundMe deployFundMe = new DeployFundMe();
     fundMe = deployFundMe.deployForTest();
+    vm.deal(USER, STARTING_BALANCE);
   }
 
   function testMinimunDollarIsFive() public {
     assertEq (fundMe.MINIMUM_USD(), 5e18);
   }
 
-  function testOwnerIsMsgSender() public {
-    assertEq(fundMe.i_owner(), address(this));
-  }
+  // function testOwnerIsMsgSender() public {
+  //   assertEq(fundMe.i_owner(), address(this));
+  // }
 
 // What can we do to work with addresses outside our system?
 // 1. Unit 
@@ -34,5 +39,21 @@ contract FundMeTest is Test {
     uint256 version = fundMe.getVersion();
     assertEq(version, 4);
   }
+
+  function testFundFailsWithoutEnoughEth() public {
+    vm.expectRevert(); // next line should revert
+    // assert(this tx fails/reverts)
+    fundMe.fund();
+  }
+  
+  function testFundUpdatedDataStructure() public {
+   vm.prank(USER);
+   fundMe.fund {value: SEND_VALUE}();
+
+   uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+   assertEq(amountFunded, SEND_VALUE);
+  }
+
+  
 }
 
